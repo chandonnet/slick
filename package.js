07a39c56061db4986314ef1d27cba6307a9344e7
Francis Chandonnet <francis.chandonnet@mcq.org>
2017-08-15 15:01:45 -0400

    package.js

---
+++ package.js
@@ -0,0 +1,21 @@
+// package metadata file for Meteor.js
+
+Package.describe({
+    name: 'fchandonnet:slick',  // http://atmospherejs.com/fchandonnet/slick
+    summary: 'Slick js skeleton wrapper for meteor',
+    version: '1.7.1',
+    git: 'https://github.com/chandonnet/slick.git'
+});
+
+
+Package.onUse(function (api) {
+    api.versionsFrom('METEOR@1.0');
+
+    api.addFiles([
+        'slick/slick.js',
+        'slick/slick.scss'
+    ], 'client');
+
+    api.use('jquery', 'client');
+    api.imply('jquery', 'client');
+});
